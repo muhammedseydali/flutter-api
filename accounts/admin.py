4d80f76cca13f3b36abbdbf4952f07e5a0f486f9
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-08-02 20:53:33 +0530

    app accounts added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
+from . import models
 from django.contrib import admin
+# Register your models here.
+
+
+class AccountAdmin(admin.ModelAdmin):
+    model = models.Account
+    list_display = ('username', 'email', 'mobile', 'is_staff', 'is_verified', 'is_active', 'last_login', 'joined_date') 
+    
+    readonly_fields = ('last_login','joined_date','password')
+    ordering = ('joined_date', )
+    filter_horizontal =()
+    list_filter = ()
+    fieldsets =()
 
 # Register your models here.
+
+admin.site.register(models.Account,AccountAdmin)
