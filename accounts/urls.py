5631734fb994aa475adae283bd4a323bebcd9008
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-08-07 11:47:42 +0530

    token auth implememnted

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from django.conf.urls import include
-from.views import RegisterView,RegistersViews, UserDetails, UsersLists,getView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-
-
+# from.views import RegisterView,RegistersViews, UserDetails, UsersLists,getView
+from .views import UserDetails, UsersLists,getView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 
 urlpatterns = [
 
-   path('register/',RegisterView.as_view(),name='register'),
-   path('registers/',RegistersViews.as_view(),name='registers'),
+#    path('register/',RegisterView.as_view(),name='register'),
+#    path('registers/',RegistersViews.as_view(),name='registers'),
    
    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
